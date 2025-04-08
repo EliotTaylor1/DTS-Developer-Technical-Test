7d7b73195a4014cea5a6717e0105769e64ffb6bc
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2025-04-08 20:40:02 +0100

    chore: Use PGSQL enum type for Task Status column

--- DTS Developer Technical Test/src/Persistence/AppDbContext.cs
+++ DTS Developer Technical Test/src/Persistence/AppDbContext.cs
@@ -5,5 +5,18 @@ namespace DTS_Developer_Technical_Test.Persistence;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
     public DbSet<Task> Tasks { get; set; }
+    
+    // use PostgreSQL enum type for Task.Status column
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresEnum<TaskStatus>(name: "task_status");
+
+        // Map the Task.Status prop to the PostgreSQL enum
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.Property(t => t.Status).HasColumnType("task_status");
+        });
+    }
 }
