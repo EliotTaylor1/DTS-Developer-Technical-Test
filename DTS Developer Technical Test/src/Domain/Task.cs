09c0547e55cc01fb5c6bc60c135fec7f1383e0af
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2025-04-08 20:03:09 +0100

    chore: make title a required field

--- DTS Developer Technical Test/src/Domain/Task.cs
+++ DTS Developer Technical Test/src/Domain/Task.cs
@@ -3,7 +3,7 @@ namespace DTS_Developer_Technical_Test.Domain;
 public class Task
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    public string Title { get; set; }
     public string? Description { get; set; }
     public TaskStatus Status { get; set; }
     public DateTime DueDate { get; set; }
