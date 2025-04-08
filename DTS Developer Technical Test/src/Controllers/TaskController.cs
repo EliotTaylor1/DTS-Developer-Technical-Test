1434a6da365dc2df7fa417050838fb8f4624d8ec
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2025-04-08 20:41:01 +0100

    feat: Create initial POST and GET endpoints

--- DTS Developer Technical Test/src/Controllers/TaskController.cs
+++ DTS Developer Technical Test/src/Controllers/TaskController.cs
@@ -1,8 +1,33 @@
+using DTS_Developer_Technical_Test.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DTS_Developer_Technical_Test.Controllers;
 
-public class TaskController : ControllerBase
+[ApiController]
+[Route("api/[controller]")]
+public class TaskController(TaskService taskService) : ControllerBase
 {
+    private readonly TaskService _taskService = taskService;
     
+    [HttpPost]
+    public IActionResult Create(Task task)
+    {
+        var createdTask = _taskService.Create(task);
+        return CreatedAtAction(
+            nameof(GetTask),
+            new { id = createdTask.Id },
+            createdTask
+            );
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetTask(int id)
+    {
+        var task = _taskService.GetById(id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+        return Ok(task);
+    }
 }
--- DTS Developer Technical Test/src/Services/TaskService.cs
+++ DTS Developer Technical Test/src/Services/TaskService.cs
@@ -2,11 +2,20 @@ using DTS_Developer_Technical_Test.Persistence;
 
 namespace DTS_Developer_Technical_Test.Services;
 
-public class TaskService
+public class TaskService(AppDbContext context)
 {
-    private readonly AppDbContext _context;
-    TaskService(AppDbContext context)
+    private readonly AppDbContext _context = context;
+
+    public Task Create(Task task)
     {
-        _context = context;
+        _context.Tasks.Add(task);
+        _context.SaveChanges();
+        return task;
+    }
+
+    public Task? GetById(int id)
+    {
+        var task = _context.Tasks.Find(id);
+        return task;
     }
 }
